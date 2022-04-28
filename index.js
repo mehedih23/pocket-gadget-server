722d74d80d48032f55147f2a79e6af20502125a5
mehedih23 <nmehedi23@gmail.com>
2022-04-29 01:09:51 +0600

    get api for products

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pocket-gadget.un9wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log("Database connected")
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const pocketGadgetCollection = client.db('pocket-gadget-store').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = pocketGadgetCollection.find(query);
+            const result = await cursor.limit(6).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
