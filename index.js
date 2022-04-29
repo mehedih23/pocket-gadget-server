a32b171317c16e8713c0ec2d1430bb25463355ea
mehedih23 <nmehedi23@gmail.com>
2022-04-29 16:44:03 +0600

    inventory api added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome To Server with mongodb')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pocket-gadget.un9wa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -22,9 +26,16 @@ async function run() {
             const result = await cursor.limit(6).toArray();
             res.send(result);
         })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await pocketGadgetCollection.findOne(query);
+            res.send(product);
+        })
     }
     finally {
-
+        // await client.close();
     }
 }
 run().catch(console.dir);
